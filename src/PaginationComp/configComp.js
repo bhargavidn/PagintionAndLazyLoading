480ed50dfdd910d7c87e04d413071bdf48c43369
Bhargavi <35219034+bhargavidn@users.noreply.github.com>
2019-05-24 18:04:50 +0530

    changes added

---
+++ src/PaginationComp/configComp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class extends Component {
+    render() {
+        return (
+            <div>Cofiguration</div> 
+        )
+    }
+}
--- src/PaginationComp/index.js
+++ src/PaginationComp/index.js
@@ -1,14 +1,22 @@
 import React,{Component} from 'react';
 import {pageData} from './pageMockData';
-
+import PageNumberComp from './pageNumberComp';
+import configComp from './configComp';
 
 export default class extends Component {
     renderPageData = () =>  pageData.map((item) => (   <div>  {item.title}  </div> ));            
   
     render() {
         return (
-            <div>
-                {this.renderPageData()}
+            <div className="container">
+                <div className="col">
+                    <configComp />
+                </div>
+                <div className="col">
+                    {this.renderPageData()}
+                
+                    <PageNumberComp />
+                </div>
             </div>
         )
     }
